b62e70bd6a9d19d8e05abee31ea1e5343a4b0193
LJB <50619527+Skyteam678@users.noreply.github.com>
2024-04-18 10:35:41 -0400

    Update TigerApplication.java

--- src/main/java/edu/Northeastern/CS5200/finalProject/TigerApplication.java
+++ src/main/java/edu/Northeastern/CS5200/finalProject/TigerApplication.java
@@ -15,8 +15,8 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 public class TigerApplication {
     public static void main(String[] args) {
         SpringApplication.run(TigerApplication.class, args);
-        log.info("项目启动成功! [Tiger Takeaway]");
-        log.info("后台管理系统默认链接: http://localhost:8080/backend/index.html");
-        log.info("前台管理系统默认链接: http://localhost:8080/front/index.html");
+        log.info("Project started successfully! [Tiger Takeaway]");
+        log.info("Backend management system: http://localhost:8080/backend/index.html");
+        log.info("Frontend service page: http://localhost:8080/front/index.html");
     }
 }
